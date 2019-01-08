617c529d43648272b362dbc7fbaef9759fa1c31f
misterobot404 <40695172+misterobot404@users.noreply.github.com>
2019-01-08 23:19:57 +1000

    6.1

--- Variant9/Lab3/Lab3.cs
+++ Variant9/Lab3/Lab3.cs
@@ -4,7 +4,48 @@ namespace Lab3
 {
     class Lab3
     {
+        public static double One(double x)
+        {
+            double y = 0;
+            if (x < -9 || x > 7) { return double.NaN; }
+            else
+            {
+                if (x >= -9 && x <= -7) y = 0;
+                if (x > -7 && x < -3) y = (-4 * x -28)/-4;
+                if (x >= -3 && x <= -2) y = 4;
+                if (x > -2 && x <= 2) y = x * x; 
+                if (x > 2 && x < 4) y = (4 * x - 16) /-2;
+                if (x >= 4 && x <= 7) y = 0;
+                return y;
+            }
+        }
         static void Main()
+        {
+            Console.WriteLine("Введите X начальное (xn), X конечное (xk) и шаг dx:\n");
+
+            double xn = double.Parse(Console.ReadLine());
+            double xk = double.Parse(Console.ReadLine());
+            double dx = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("          Таблица Значений Функции");
+            Console.WriteLine("\n_____________________________________________\n");
+            Console.WriteLine("|         x           |          f(x)        |" +
+                              "\n_____________________________________________\n");
+
+            double x = xn;
+            while (x <= xk)
+            {
+                Console.WriteLine("        {0}             {1}      ", x, One(x));
+                x += dx;
+            }
+            Console.WriteLine("\n______________________________________________");
+            Console.ReadLine();
+        }
+    }
+
+    class Task3
+    {
+        Task3()
         {
             double eps, x, xlim, dx;
             do
@@ -31,14 +72,14 @@ namespace Lab3
                 int iter = 0;
                 for (; Math.Abs(Math.Atan(x) - s) > eps && iter < 1000; n++, iter++)
                 {
-                    if((x % 2) == 0)
+                    if ((x % 2) == 0)
                     {
                         s = s - a;
                     }
                     else
                     {
                         s = s + a;
-                    }                  
+                    }
                     a = (2 * n + 1) / (2 * n + 1);
                 }
                 if (s == 0) s = x;
